80d873612568758a5072769c667da3715d407370
bikki043 <bigckung064@gmail.com>
2025-12-16 15:05:31 +0700

    LAB5

--- php/condb.php
+++ php/condb.php
@@ -4,8 +4,8 @@ $user = "root";
 $pass = "1234";
 $db   = "mydb";
 try{
-    $conn = new mysqli($host, $user, $pass, $db);
-    if($conn->connect_error){
+    $condb = new mysqli($host, $user, $pass, $db);
+    if($condb->connect_error){
         throw new Exception("Connection failed: ");
     }
 }
